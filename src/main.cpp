291ccc0d712b73ef9f13e68359081149e8fac85c
Matteo Galiazzo <mattegaliz@gmail.com>
2022-11-17 16:14:55 +0100

    added << overload for date and refactored date variables

--- src/main.cpp
+++ src/main.cpp
@@ -3,15 +3,30 @@
 #include "date.h"
 #include "book.h"
 
+using namespace std;
+
 int main() {
     /* 
      * DATE TESTING
      */
+    cout << "=== DATES TESTS ===" << endl;
     Date default_date{};
-    std::cout << default_date.day() << " " << default_date.month() << " " << default_date.year() << std::endl;
+    cout << default_date << endl;
+    cout << "IsLeapYear(2022) -> " << Date::IsLeapYear(2022) << endl;
+    cout << "IsLeapYear(2012) -> " << Date::IsLeapYear(2012) << endl;
+    Date date1{20, Date::Month::Nov, 2001};
+    cout << date1 << endl;
+    try {
+        Date date2{30, Date::Month::Feb, 2022};
+    } catch (Date::DATE_invalid){
+        cout << "Data invalida";
+    }
+    cout << endl;
+
     /*
      * MINIMAL TESTING
      */
+    cout << "=== BOOKS TESTS ===" << endl;
     std::vector<Book> shelf(10);
     //Book my_favourite_book("David", "Foster Wallace", "Una cosa divertente che non farò mai più", "887-521-837-4");
 
